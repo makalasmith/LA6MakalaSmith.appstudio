a953647255e718ee2265d7de520722cb12c3031f
Makala Smith <mms96354@creighton.edu>
2021-03-11 11:50:06 -0600

    input name

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,3 +1,11 @@
 
 let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
 
+
+btnSubmit.onclick=function(){
+  let name = inptName.value
+  if members.includes(name) 
+    lblMessage.textContent = `Hello ${name}, you are a member`
+  else
+    lblMessage.textContent = `Hello ${name}, you are not a member`
+}
