a186de5a71584392547044db3a8fabc1e51ccd05
Makala Smith <mms96354@creighton.edu>
2021-03-15 09:59:04 -0500

    fixed

--- forms/calculator/calculator.js
+++ forms/calculator/calculator.js
@@ -3,10 +3,10 @@ var answer = 0
 
 
 btnAdd.onclick=function(){
-  let num1 = parsInt(inptNumber1.value)
-  let num2 = parsInt(inptNumber2.value)
+  let num1 = parseInt(inptNumber1.value)
+  let num2 = parseInt(inptNumber2.value)
   let answer = num1 + num2
-  lblAnswer = answer
+  lblAnswer.value = answer
 }
 
 btnClear.onclick=function(){
